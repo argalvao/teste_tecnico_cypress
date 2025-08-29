533c06a162cdd786b9d41b46aabbfdd8352dda37
Abel Galvão <abel.ramalho18@gmail.com>
2025-08-29 18:31:49 -0300

    Caso de teste CT06

--- cypress/e2e/booking_reservation.cy.js
+++ cypress/e2e/booking_reservation.cy.js
@@ -9,7 +9,7 @@ const formatDate = (date) => {
 describe('Fluxos de Reserva de Quarto', () => {
 
 
-  
+  /*
   // Caso de teste CT05
   it('Realizar reserva via request', () => {
 
@@ -48,6 +48,41 @@ describe('Fluxos de Reserva de Quarto', () => {
     cy.log('Verificação concluída: Reserva efetuada via request.');
 
   });  
-  
+  */
+
+  // Caso de teste CT06
+  it('Realizar reserva via formulário', () => {
+
+    // Criação das datas dinâmicas para a reserva
+    const today = new Date(); // data atual
+    const checkinDate = formatDate(today);
+    const dayAfterTomorrow = new Date();
+    dayAfterTomorrow.setDate(today.getDate() + 2);
+    const checkoutDate = formatDate(dayAfterTomorrow);
+
+
+    cy.log(`Preparando o estado: criando uma reserva via formulário de ${checkinDate} a ${checkoutDate}`);
+
+    // Realizando reserva via formulário
+    cy.visit('https://automationintesting.online/');
+    cy.viewport(1280, 720);
+
+    // Clicando no botão de reservar
+    cy.get(':nth-child(3) > .card > .card-footer > .btn').click({ force: true });
+
+    cy.get('#doReservation').click({ force: true });
+
+    // Preenchendo o formulário
+    cy.get('[placeholder="Firstname"]').scrollIntoView().type('Abel', {force: true});
+    cy.get('[placeholder="Lastname"]').type('Ramalho', {force: true});
+    cy.get('[placeholder="Email"]').type('abel.ramalho@gmail.com', {force: true});
+    cy.get('[placeholder="Phone"]').type('75992148111', {force: true});
+    // Finalizando a reserva
+    cy.contains('button', 'Reserve Now').should('be.visible').should('not.be.disabled').click({ force: true });
+
+    // Conclusão do teste
+    cy.log('Verificação concluída: Reserva efetuada via formulário.');
+
+  });
 
 });
