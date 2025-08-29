214cd43ed04a72d32a25b34f82d725ece93073de
Abel Galvão <abel.ramalho18@gmail.com>
2025-08-29 08:07:10 -0300

    Caso de teste CT03

--- cypress/e2e/booking.cy.js
+++ cypress/e2e/booking.cy.js
@@ -34,7 +34,6 @@ describe('Fluxo de Envio de Contato', () => {
     // Verificar mensagem de sucesso
     cy.get('h3.h4.mb-4').should('contain.text', 'Thanks for getting in touch Abel Ramalho Galvão!');
   });
-  */
 
   // Caso de teste CT02
   it('Não deve enviar a mensagem quando o campo obrigatório "Name" está vazio', () => {
@@ -60,4 +59,34 @@ describe('Fluxo de Envio de Contato', () => {
     // Conclusão do teste
     cy.log('Verificação concluída: A chamada para a API não foi realizada, como esperado.');
   });
+  */
+
+  // Caso de teste CT03
+  it('Não deve enviar a mensagem com campo "Subject" abaixo do tamanho mínimo', () => {
+
+    cy.intercept('POST', 'api/message').as('postMessage');
+
+    // Preenchemos todos os campos do formulário
+    cy.get('[data-testid="ContactName"]').scrollIntoView().type('Abel Ramalho Galvão', { force: true });
+    cy.get('[data-testid="ContactEmail"]').type('abel.ramalho18@gmail.com', { force: true });
+    cy.get('[data-testid="ContactPhone"]').type('75992148111', { force: true });
+
+    // Usando um assunto com 4 caracteres, que é inválido (mínimo é 5)
+    cy.get('[data-testid="ContactSubject"]').type('1234', { force: true }); 
+    
+    cy.get('[data-testid="ContactDescription"]').type('Esta mensagem testa a validação de tamanho mínimo do campo de assunto');
+    
+    cy.contains('button', 'Submit').click();
+
+    // Verificando que a chamada para a API não foi realizada
+    cy.wait('@postMessage').its('response.statusCode').should('eq', 400);
+
+    // Verificando que não houve a mensagem de sucesso
+    cy.get('.alert').should('not.contain', 'Thanks for getting in touch');
+
+    // Conclusão do teste
+    cy.log('Verificação concluída: O campo não aceita menos de 5 caracteres.');
+  });
+
+
 });
